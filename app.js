4ab4dbc5f21936bebe4e82310575b5f1089878d0
ZachCharlick <39837560+ZachCharlick@users.noreply.github.com>
2019-07-10 16:29:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -164,8 +164,8 @@ app.get('/editProfile',isLoggedIn, (req,res)=>{
   res.render('editProfile')
 })
 
-app.get('/Chats',isLoggedIn, (req,res)=>{
-  res.render('Chats')
+app.get('/chats',isLoggedIn, (req,res)=>{
+  res.render('chats')
 })
 
 app.get('/interests', (req,res)=>{
