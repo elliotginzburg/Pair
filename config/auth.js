985ff4aa9275011a379ed9a3f0cc4dffb3e04174
Jaivd7 <42908626+Jaivd7@users.noreply.github.com>
2019-07-11 14:41:22 -0400

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@ module.exports = {
 
 
     'googleAuth' : {
-        'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
-        'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
+        'clientID': '468514357077-829349bub13mh8l8l1fj04e6qbr3q6hu.apps.googleusercontent.com',
+        'clientSecret'  : 'orCSTBfnXyKuK_50BPbFdmnE',
         'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
     }
 
