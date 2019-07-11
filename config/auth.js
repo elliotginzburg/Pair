c4c407be34c05acbc146d6f30181b26358fef821
ZachCharlick <39837560+ZachCharlick@users.noreply.github.com>
2019-07-11 15:17:13 -0400

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@ module.exports = {
 
 
     'googleAuth' : {
-        'clientID': '468514357077-829349bub13mh8l8l1fj04e6qbr3q6hu.apps.googleusercontent.com',
-        'clientSecret'  : 'orCSTBfnXyKuK_50BPbFdmnE',
+        'clientID': '738740318135-56ced1ko1ttkdt58l6gg23udhhlmejus.apps.googleusercontent.com',
+        'clientSecret'  : 'nSFS0akBCTvN6E0-w2g1G9Sa',
         'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
     }
 
