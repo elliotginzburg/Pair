84b41e883853bf3d632980ac4413cf2b3cedf493
MQuetell <52639030+MQuetell@users.noreply.github.com>
2019-07-11 10:46:19 -0400

    sd

--- app.js
+++ app.js
@@ -164,7 +164,7 @@ app.get('/aboutpages', function(req, res) {
           res.render('aboutpages')
 });
 
-// CHAT CHANGE 
+// CHAT CHANGE
 app.get('/chat',(req,res,next)=>{
   res.render('chat',{title:"ChatDemo"});
 })
