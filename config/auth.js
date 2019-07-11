e4a6d996b3f933c5cc4e727ea7337944673ba8e2
tim <tjhickey@brandeis.edu>
2019-07-11 11:51:59 -0400

    heroku changes

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,8 @@ module.exports = {
     'googleAuth' : {
         'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
         'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
-        'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
+        'callbackURL': 'https://dry-harbor-12685.herokuapp.com/'
+        //'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
     }
 
 };
