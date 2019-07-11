eb41efb220d2f4e1417e3824f07842d234122a50
tim <tjhickey@brandeis.edu>
2019-07-11 11:54:33 -0400

    fixed callback

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
         'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
-        'callbackURL': 'https://dry-harbor-12685.herokuapp.com/'
+        'callbackURL': 'https://dry-harbor-12685.herokuapp.com/login/authorized'
         //'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
     }
 
