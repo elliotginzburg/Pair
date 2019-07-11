718447876af0e79e0a9479ba4274c6ee139e19aa
ZachCharlick <39837560+ZachCharlick@users.noreply.github.com>
2019-07-11 15:22:48 -0400

    added new config

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '738740318135-56ced1ko1ttkdt58l6gg23udhhlmejus.apps.googleusercontent.com',
         'clientSecret'  : 'nSFS0akBCTvN6E0-w2g1G9Sa',
-        'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
+        'callbackURL'   : 'https://obscure-cliffs-65103.herokuapp.com/login/authorized'
     }
 
 };
