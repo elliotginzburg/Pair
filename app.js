4e406e88257951e94acf5213c4184758fe8fa552
ZachCharlick <39837560+ZachCharlick@users.noreply.github.com>
2019-07-12 20:30:51 -0400

    Switch back to local host

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var uristring =
     process.env.MONGOLAB_GOLD_URI ||
     process.env.MONGOHQ_URL ||
     // pick localhost or mlab
-    //'mongodb://localhost/mydb';
-    'mongodb://heroku_1mh6jvp2:mggno2vrjh2036n5tf58ppqh7t@ds247637.mlab.com:47637/heroku_1mh6jvp2';
+    'mongodb://localhost/mydb';
+    //'mongodb://heroku_1mh6jvp2:mggno2vrjh2036n5tf58ppqh7t@ds247637.mlab.com:47637/heroku_1mh6jvp2';
 
     // Makes connection asynchronously.  Mongoose will queue up database
     // operations and release them when the connection is complete.
@@ -32,8 +32,8 @@ var uristring =
       }
     });
 
-
-//mongoose.connect( 'mongodb://localhost/mydb', { useNewUrlParser: true } );
+// for localhost
+mongoose.connect( 'mongodb://localhost/mydb', { useNewUrlParser: true } );
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
