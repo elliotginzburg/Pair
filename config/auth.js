9e9666b9dbf8231395e9cda8ec08476167c27bfe
ZachCharlick <39837560+ZachCharlick@users.noreply.github.com>
2019-07-16 09:22:45 -0400

    wd

--- config/auth.js
+++ config/auth.js
@@ -12,8 +12,8 @@ module.exports = {
 	// Zach's heroku server
         'clientID': '738740318135-56ced1ko1ttkdt58l6gg23udhhlmejus.apps.googleusercontent.com',
         'clientSecret'  : 'nSFS0akBCTvN6E0-w2g1G9Sa',
-        //callbackURL'   : 'https://obscure-cliffs-65103.herokuapp.com/login/authorized'
-        'callbackURL'   : 'http://localhost:5500/login/authorized'
+        'callbackURL'   : 'https://obscure-cliffs-65103.herokuapp.com/login/authorized'
+        //'callbackURL'   : 'http://localhost:5500/login/authorized'
     }
 
 };
